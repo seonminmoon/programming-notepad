2b2943f4d6daa874825999de1e4af54a32cf98fa
문선민 <seonminmoon@gmail.com>
2021-11-28 23:57:20 +0900

    test

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -31,23 +31,15 @@ function LoginPage() {
     }
     return (
         <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100%'}}>
-
-
-
             <form style={{display: 'flex', flexDirection:'column'}} onSubmit={onSubmitHandler}>
                 <label>Email</label>
-                <input type="email" value={Email} onChange={onEmailHandler}/>
+                {/* onChange로 setEmail() 실행 => state값 변경됨 => 변경된 state에 따라 email 값이 전달됨 => value 변경 */}
+                <input type="email" value={Email} onChange={onEmailHandler}/> 
                 <label>Password</label>
                 <input type="password" value={Password} onChange={onPasswordHandler}/>
                 
-                <button>
-                    Login
-                </button>
+                <button>Login</button>
             </form>
-
-
-
-
         </div>
     )
 }
